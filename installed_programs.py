66ab27846a0c29cdbecb585bcf2d1f23aa394efd
hadrianhu888 <hadrian.hu@gmail.com>
2023-09-03 14:14:50 -0400

    Removed unnecessary files

--- installed_programs.py
+++ installed_programs.py
@@ -2,7 +2,7 @@ import subprocess
 
 def save_installed_programs_windows(filename):
     with open(filename, 'w') as file, open('error_log.txt', 'w') as error_file:
-        completed_process = subprocess.run(['wmic', 'product', 'get', 'name'], stdout=file, stderr=error_file, text=True)
+        completed_process = subprocess.run(['C:\\Windows\\System32\\wbem\\wmic', 'product', 'get', 'name'], stdout=file, stderr=error_file, text=True)
         
         # Check return code (0 usually indicates success)
         print("Return code:", completed_process.returncode)
