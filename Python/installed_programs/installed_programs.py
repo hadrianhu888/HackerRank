45d3a5d8e74ea44e241286a64c104b06f2638ab3
hadrianhu888 <hadrian.hu@gmail.com>
2023-09-23 20:55:43 -0400

    modified installed_programs

--- Python/installed_programs/installed_programs.py
+++ Python/installed_programs/installed_programs.py
@@ -1,32 +1,60 @@
 import winreg
-import contextlib
-class InstalledPrograms: 
 
-    def __init__(self):
-        """_summary_
-        """
-        pass
 
-    def get_installed_programs(self):
-        installed_programs_list = []
-        registry_path = r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+def installed_programs():
+    
+    
+    """
+    Executes the installed programs retrieval process.
 
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, registry_path) as key:
-            for i in range(winreg.QueryInfoKey(key)[0]):
-                subkey_name = winreg.EnumKey(key, i)
-                with winreg.OpenKey(key, subkey_name) as subkey:
-                    with contextlib.suppress(FileNotFoundError):
-                        program_name = winreg.QueryValueEx(subkey, "DisplayName")[0]
-                        installed_programs_list.append(program_name)
+    Opens the Uninstall key in the Windows registry and iterates over all subkeys to find the DisplayName value of each program. Prints the DisplayName value if it exists.
 
-        with open("installed_programs_windows.txt", "w") as f:
-            for program in installed_programs_list:
-                f.write(program + "\n")
-                
-def main():
-    installed_programs_obj = InstalledPrograms()
-    installed_programs_obj.get_installed_programs()
+    Args:
+        None
 
-if __name__ == "__main__":
-    main()
-    print("List of installed programs has been saved to installed_programs_windows.txt")
+    Returns:
+        None
+
+    Raises:
+        OSError: Raised when an error occurs while accessing the registry.
+
+    Example:
+        ```python
+        installed_programs()
+        ```
+    """
+
+    # Open the Uninstall key.
+    # The comment `# Open the Uninstall key.` is describing the purpose of the code line that follows
+    # it. In this case, the code line `key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+    # r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", 0, winreg.KEY_READ |
+    # winreg.KEY_WOW64_64KEY)` is opening the Uninstall key in the Windows registry. This key contains
+    # information about the installed programs on the system.
+    key = winreg.OpenKey(
+        winreg.HKEY_LOCAL_MACHINE,
+        r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+        0,
+        winreg.KEY_READ | winreg.KEY_WOW64_64KEY
+    )
+
+    # Iterate over all subkeys.
+    for i in range(0, winreg.QueryInfoKey(key)[0]):
+        skey_name = winreg.EnumKey(key, i)
+        skey = winreg.OpenKey(key, skey_name)
+        try:
+            # Look for the DisplayName value.
+            disp_name = winreg.QueryValueEx(skey, 'DisplayName')[0]
+            print(disp_name)
+        except OSError:
+            pass
+        finally:
+            skey.Close()
+
+    key.Close()
+    
+    
+def main(): 
+    installed_programs()
+    
+
+
